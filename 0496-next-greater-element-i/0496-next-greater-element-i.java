034c816e0c6f08e9f9cead46a41fcae522d06fd0
Shaurya Rathore <pradhansahabb2004@gmail.com>
2026-03-17 21:57:39 +0530

    Time: 3 ms (92.62%), Space: 45.3 MB (57.44%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -5,7 +5,7 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         Stack<Integer> stack = new Stack<>();
 
-        // Build next greater map for nums2
+       
         for (int num : nums2) {
             while (!stack.isEmpty() && stack.peek() < num) {
                 map.put(stack.pop(), num);
@@ -13,12 +13,12 @@ class Solution {
             stack.push(num);
         }
 
-        // Elements left in stack have no greater element
+       
         while (!stack.isEmpty()) {
             map.put(stack.pop(), -1);
         }
 
-        // Build result for nums1
+       
         int[] ans = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
             ans[i] = map.get(nums1[i]);
